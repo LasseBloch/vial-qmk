ab11c46dff580d240a744615979febe8373d799b
Lasse Bloch Lauritsen <lassebloch@gmail.com>
2023-04-21 20:24:14 +0200

    config for Elite-pi, needed to be explicit about the SPI pins

--- keyboards/handwired/wylderbuilds/trackball/5x6_track/config.h
+++ keyboards/handwired/wylderbuilds/trackball/5x6_track/config.h
@@ -8,18 +8,8 @@
 #define WYLD_QMK_FIRMWARE_CONFIG_H
 
 #endif // WYLD_QMK_FIRMWARE_CONFIG_H
-#define BOOTMAGIC_LITE_ROW 0        // TOP RIGHT KEY ON RIGHT SIDE, TOP LEFT KEY ON LEFT SIDE
-#define BOOTMAGIC_LITE_COLUMN 5
-
-//#define PRODUCT_ID 0x0002
-
 #define USE_SERIAL
 #define SPLIT_HAND_PIN      GP26  // high = left, low = right
-//#define	SPLIT_HAND_PIN_LOW_IS_LEFT
-//#define SPLIT_HAND_MATRIX_GRID GP9, GP8
-//#define MASTER_RIGHT
-//#define MASTER_LEFT
-
 
 // Using Serial instead of I2C
 #define SERIAL_USART_FULL_DUPLEX 
@@ -40,49 +30,32 @@
 #define MATRIX_COLS 6
 
 #define MATRIX_COL_PINS { GP3, GP4, GP5, GP6, GP7, GP8 }
-#define MATRIX_ROW_PINS { GP10, GP11, GP12, GP13, GP14, GP15 }
-
+#define MATRIX_ROW_PINS { GP9, GP12, GP13, GP14, GP15, GP16 }
 #define DIODE_DIRECTION COL2ROW
 
+/* SPI & PMW3360 settings. */
+#define SPI_DRIVER SPID0
+#define SPI_SCK_PIN GP22
+#define SPI_MISO_PIN GP20
+#define SPI_MOSI_PIN GP23
+#define POINTING_DEVICE_CS_PIN GP21
 #define SPLIT_POINTING_ENABLE
 #define ROTATIONAL_TRANSFORM_ANGLE  -25
 #define POINTING_DEVICE_INVERT_Y
 #define POINTING_DEVICE_RIGHT
-#define PMW33XX_CS_PIN GP21
 #define POINTING_DEVICE_TASK_THROTTLE_MS 1
-#define PMW33XX_LIFTOFF_DISTANCE 0x02
+#define PMW33XX_LIFTOFF_DISTANCE 0x05
 
 #define SPLIT_TRANSACTION_IDS_KB RPC_ID_KB_CONFIG_SYNC
 
-#define CHARYBDIS_MINIMUM_DEFAULT_DPI 1200
-#define CHARYBDIS_DEFAULT_DPI_CONFIG_STEP 400
-#define CHARYBDIS_MINIMUM_SNIPING_DPI 400
-#define CHARYBDIS_SNIPING_DPI_CONFIG_STEP 200
-
-//// Rotary Encoder ////
-// GP21
-// GP22
 
 //// Sensor Notes ////
 //// Pi Pico pins ////
-// 3V3  -- Power (not 3v3_EN!)
-// GP21 -- CS pin
+// 3V3  -- Power (not 3v3_EN)
+// GP17 -- CS pin
 // GP20 -- MISO pin
 // GP19 -- MOSI pin
-// GP18 -- SCK pin clock
-// GND  -- GND
-
-// WS2812 RGB LED strip input and number of LEDs
-//#ifdef RGBLIGHT_ENABLE
-//#define RGB_DI_PIN GP17
-//#define RGBLED_NUM 34
-////#define DRIVER_LED_TOTAL 34
-//#define RGBLED_SPLIT { 17, 17 }
-//#define STM32_SYSCLK KINETIS_SYSCLK_FREQUENCY
-//#define NOP_FUDGE 0.4
-//#endif
-
-//// OLED Pins and Driver
+// GP18 -- SCK pin clock Driver
 #ifdef OLED_ENABLE
 #define I2C_DRIVER I2CD1
 #define I2C1_SDA_PIN GP12
@@ -105,7 +78,6 @@
 // Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap
 #define LOCKING_SUPPORT_ENABLE
 // Locking resynchronize hack
-#define LOCKING_RESYNC_ENABLE
-// Enables This makes it easier for fast typists to use dual-function keys
+#define LOCKING_RESYNC_ENABLE// Enables This makes it easier for fast typists to use dual-function keys
 #define PERMISSIVE_HOLD
 // End of Basic Config
